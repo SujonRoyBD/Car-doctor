0d2c758394ffc95e39dcf9d9d039844fff26ca98
SujonRoyBD <sujonroy537527@gmail.com>
2025-01-16 23:12:40 +0600

    time

--- src/app/clock/Clock.js
+++ src/app/clock/Clock.js
@@ -8,14 +8,14 @@ const Clock = () => {
                 <Image src="/aseats/ghori.png" alt='clock' height={20} width={30} className='h-9 ml-4 mt-3 '/>
                 <div>
                    <p>We are open monday-friday</p> 
-                   <h3  className='text-2xl font-bold'>7:00 am - 9:00 pm</h3>
+                   <h3  className='text-2xl font-bold'>7:00 am - 8:00 pm</h3>
                 </div>
             </div>
             <div className='flex gap-4 mt-7'>
                 <Image src="/aseats/ghori.png" alt='clock' height={20} width={30} className='h-9 ml-4 mt-3 '/>
                 <div>
                    <p>We are open monday-friday</p> 
-                   <h3  className='text-2xl font-bold'>7:00 am - 9:00 pm</h3>
+                   <h3  className='text-2xl font-bold'>7:00 am - :800 pm</h3>
                 </div>
             </div>
             <div className='flex gap-4 mt-7'>
