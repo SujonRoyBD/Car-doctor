be5bb94aa201d8823bc8d4b6c9c162f520ce2779
SujonRoyBD <sujonroy537527@gmail.com>
2025-02-08 22:29:00 +0600

    text

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`overflow-x-hidden ${geistSans.variables} ${geistMono.variabl} antialiased`}
+        className={`overflow-x-hidden ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
         {children}
