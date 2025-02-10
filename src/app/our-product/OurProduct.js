82d90ae207e987066e6a14193e2fe9fcb5258ca5
SujonRoyBD <sujonroy537527@gmail.com>
2025-02-10 21:10:46 +0600

    gusd

--- src/app/our-product/OurProduct.js
+++ src/app/our-product/OurProduct.js
@@ -80,7 +80,7 @@ const OurProduct = () => {
         <p className='text-center'>
           the majority have suffered alteration in somes form, by injected humour, or randomised words which
           <br />
-          dont look eves slightly believabls.
+          dont look eves slightly believabl.
         </p>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-5'>
